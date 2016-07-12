f75294580341e96a15b11eed59b9d4d0bc3b2767
zyy <zyyjade@126.com>
2016-07-12 23:43:24 +0800

    cc

--- static/telltale/index/index.js
+++ static/telltale/index/index.js
@@ -2,11 +2,13 @@
 * @Author: jade
 * @Date:   2016-07-05 19:23:10
 * @Last Modified by:   jade
-* @Last Modified time: 2016-07-10 21:12:57
+* @Last Modified time: 2016-07-12 23:27:55
 */
 
 'use strict';
 (function($){
+    var winW = $(window).width();
+    var winH = $(window).height();
     $('.page1').css({
         height: $(window).height() - 78
     })
@@ -33,5 +35,17 @@
             alert('success')
             $('.mask, .dialog').hide();
         }
+    })
+
+    var p3_1W = winW * 0.26;
+    var p3_1H = winW * 0.26/1.68;
+    $('.p3_1').css({
+        width: p3_1W,
+        height: p3_1H,
+        'margin-left': -p3_1W/2,
+        'margin-top': -p3_1H/2
+    });
+    $('.p3_2').css({
+        'margin-top': p3_1H * 0.6
     })
 })(jQuery);
